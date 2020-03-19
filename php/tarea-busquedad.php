ca40c19976cdab7e973f0d290ab37ba6dca006e9
ISMAEL GARCIA <fging2019@gmail.com>
2020-03-19 15:56:44 -0430

    modelo mysqli

--- php/tarea-busquedad.php
+++ php/tarea-busquedad.php
@@ -1,12 +1,20 @@
 <?php
  
- include_once('db.php');
+ include('db.php');
 
  $buscar=$_POST['buscar'];
  
  if (!empty($buscar)) {
      $q="SELECT * FROM tareas WHERE nombre LIKE '$search%'";
+     $r= mysqli_query($conex,$q);
+     if(!$r){
+         die('Error sentencia'.mysqli_error($conex));
+     }
+     $json=array();
+  while ($row =mysqli_fetch_array($r)) {
      
+  }
+
  }
 
 
