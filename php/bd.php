71cd7e7059e7dbfa4adf8f9ea5293140911f0c9b
ISMAEL GARCIA <fging2019@gmail.com>
2020-03-19 14:37:56 -0430

    modelo mysqli

--- php/bd.php
+++ php/bd.php
@@ -1,29 +1,5 @@
 <?php
     
-//CREACION DEL OBJETO CONEXION
-
-    class conexion {
-
-        function conectar(){
-        $host="mysql:host=localhost";
-        $db="dbname=tareasbd";
-        $user="root";
-        $pass="";
-            try {				    	
-         $conex= new PDO($host.";".$db,$user,$pass);					    
-         $conex->exec("SET CHARACTER SET utf8");
-         
-         } catch (Exception $e) {
-            echo 'error en la conexion';
-                     
-                 }
-                 return $conex;
-        }
-    }
-
-//DECLARACION DEL OBJETO CONEXION    
-$con = new conexion();
-        $conex     = $con->conectar();
 
 
 ?>
